98ab5f701ea330af79bbc426bec9989881548b8e
Driften7620 <driftenlemvigvarmevaerk@gmail.com>
2026-06-25 13:24:14 +0200

    Page fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/DriftPortal-2.0/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
